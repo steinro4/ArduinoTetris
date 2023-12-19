86a62f0988e01db206e869094cdfefc65bddc918
SIMON-PC\Simon <pfister.simi@gmail.com>
2023-12-19 21:54:19 +0100

    added: getNewPiece, movePiece - methods

--- Source/Main/Main.ino
+++ Source/Main/Main.ino
@@ -10,13 +10,13 @@ struct Coordinates{
 Coordinates *movingPiece;
 
 //Pieces
-Coordinates RightZPiece[4] = {3,-1,5,-2,4,-2,4,-1};
 Coordinates RightLPiece[4] = {3,-3,3,-2,3,-1,4,-1};
 Coordinates LeftLPiece[4] = {4,-3,4,-2,4,-1,3,-1};
 Coordinates LongPiece[4] = {4,-4,4,-3,4,-2,4,-1};
 Coordinates SquarePiece[4] = {4,-2,3,-2,4,-1,3,-1};
 Coordinates TPiece[4] = {5,-2,3,-2,4,-2,4,-1};
 Coordinates LeftZPiece[4] = {5,-1,3,-2,4,-2,4,-1};
+Coordinates RightZPiece[4] = {3,-1,5,-2,4,-2,4,-1};
 
 //******************************************************************
 //Static variables
@@ -30,11 +30,12 @@ bool field[8][16] = { {} };
 void setup() {
   //******************************************************************
   //Dynamic setup
+  movingPiece = getNewPiece();
+
+
 
   hardwareInit();
   Serial.begin(9600);
-
-  movingPiece = getNewPiece();
 
   //******************************************************************
   //Static setup
@@ -53,9 +54,13 @@ void loop() {
 
   //******************************************************************
   //Dynamic Loop
+  movingPiece = moveDown(movingPiece);
   for(int i=0; i < 4; i++)
   {
     Serial.println(movingPiece[i].y);
+  }
+  if(movingPiece[0].y > 10){
+    movingPiece = getNewPiece();
   }
   delay(500);
 
@@ -67,8 +72,115 @@ void loop() {
 
 //******************************************************************
 //Dynamic Methods
+
+//*********************
+//Get new Piece
+Coordinates tempPiece[4];
 Coordinates * getNewPiece(){
-  return RightZPiece;
+
+  Coordinates *tempPointer;
+
+  switch(random(7)){
+    case 0: tempPointer = RightLPiece; break;
+    case 1: tempPointer = LeftLPiece; break;
+    case 2: tempPointer = LongPiece; break;
+    case 3: tempPointer = SquarePiece; break;
+    case 4: tempPointer = TPiece; break;
+    case 5: tempPointer = LeftZPiece; break;
+    case 6: tempPointer = RightZPiece; break;
+  }
+
+  //Make copy so the original doesn't get overwritten
+  for(int i = 0; i < 4; i++)
+  {
+      tempPiece[i] = tempPointer[i];
+  }
+
+  return tempPiece;
+}
+
+//*********************
+//Move down
+Coordinates * moveDown(Coordinates *piece){
+  for(int i = 0; i < 4; i++)
+  {
+    piece[i].y++;
+  }
+  return piece;
+}
+
+//*********************
+//Move up
+Coordinates * moveUp(Coordinates *piece){
+  for(int i = 0; i < 4; i++)
+  {
+    piece[i].y--;
+  }
+  return piece;
+}
+
+//*********************
+//Move left
+Coordinates * moveLeft(Coordinates *piece){
+  bool freeToMove = true;
+
+  for(int i = 0; i < 4; i++)
+  {
+    if(piece[i].x <= 0 || piece[i].y >= 16) //numRows
+    {
+      freeToMove = false;
+    }
+
+    if(freeToMove)
+    {
+      if(field[piece[i].x-1][piece[i].y])
+      {
+        freeToMove = false;
+      }
+    }
+  }
+
+  if(freeToMove)
+  {
+    for(int i = 0; i < 4; i++)
+    {
+      piece[i].x--;
+    }
+  }
+
+  return piece;
+}
+
+//*********************
+//Move right
+Coordinates * moveRight(Coordinates *piece){
+  bool freeToMove = true;
+
+  for(int i = 0; i < 4; i++)
+  {
+    if(piece[i].x > 7 || piece[i].y >= 16) //numRows
+    {
+      freeToMove = false;
+    }
+
+    if(freeToMove)
+    {
+      if(field[piece[i].x+1][piece[i].y])
+      {
+        freeToMove = false;
+      }
+    }
+  }
+
+  if(freeToMove)
+  {
+    for(int i = 0; i < 4; i++)
+    {
+      piece[i].x++;
+    }
+  }
+
+  return piece;
 }
 
 //******************************************************************
